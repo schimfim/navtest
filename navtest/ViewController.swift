843f32841cbfbf7887d5d71f0b523c9b4ba8ab5a
Frank Reine <frank.reine@mac.com>
2015-10-23 22:41:28 +0200

    Initial Commit

---
+++ navtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  navtest
+//
+//  Created by Frank Reine on 23.10.15.
+//  Copyright © 2015 Frank Reine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
