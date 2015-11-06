6df7f85d6b4b303c5a574f3e109fd7a4ebde05cb
Frank Reine <frank.reine@mac.com>
2015-11-06 08:18:52 +0100

    Handle default image

--- navtest/ViewController.swift
+++ navtest/ViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     // add image here
-    
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var resultImage: UIImageView!
+    var origImage: UIImage!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,5 +25,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view)
+        recognizer.view!.center = CGPoint(x:recognizer.view!.center.x + translation.x, y:recognizer.view!.center.y + translation.y)
+        recognizer.setTranslation(CGPointZero, inView: self.view)
+    }
+    
+    @IBAction func handlePinch(recognizer : UIPinchGestureRecognizer) {
+        recognizer.view!.transform = CGAffineTransformScale(recognizer.view!.transform,
+            recognizer.scale, recognizer.scale)
+        recognizer.scale = 1
+    }
 
 }
