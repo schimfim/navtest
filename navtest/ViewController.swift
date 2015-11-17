2a654eb7acab0b939e8f95e4fe22247e4cc3073e
Frank Reine <frank.reine@mac.com>
2015-11-17 22:56:32 +0100

    Refactored ScrollView handling

--- navtest/ViewController.swift
+++ navtest/ViewController.swift
@@ -12,29 +12,23 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     // add image here
     @IBOutlet var resultImage: UIImageView!
-
+    @IBOutlet var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         Filter.resultImage = resultImage
         Filter.origImage = resultImage.image!
+        
         scrollView.contentSize = Filter.origImage.size
-
         let scrollViewFrame = scrollView.frame
         let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
         let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
         let minScale = min(scaleWidth, scaleHeight);
         scrollView.minimumZoomScale = minScale;
-        
-        // 5
         scrollView.maximumZoomScale = 1.0
         scrollView.zoomScale = minScale;
-
         NSLog("zoomScale: %f", minScale)
-        
-        // 6
-        // centerScrollViewContents()
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,7 +36,9 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func viewForZoomingInScrollView
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return resultImage
+    }
     
     
 
