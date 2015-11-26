e27cb6331f599f97b6a3e4de6134cde9bdbfddc7
uncleWalter <uncleWalter@mac.com>
2015-11-26 09:38:58 +0100

    render async

--- navtest/Filter.swift
+++ navtest/Filter.swift
@@ -72,10 +72,11 @@ class Filter: NSObject, NSCoding {
         dispatch_async(dispatch_get_global_queue(QOS_CLASS_UTILITY, 0)) {
             NSLog("2 Starting async task")
             let outputImage = currentFilter!.process(inImage!)
+            let cgimg = context!.createCGImage(outputImage, fromRect: outputImage.extent)
             //sleep(5)
             dispatch_async(dispatch_get_main_queue()) {
             	NSLog("3 Dispatch to main queue")
-                let cgimg = context!.createCGImage(outputImage, fromRect: outputImage.extent)
+
                 let out = UIImage(CGImage: cgimg, scale: 1.0, orientation:.Up)
                 activity.stopAnimating()
                 Filter.resultImageView.image = out
