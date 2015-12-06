e3121fa513a85e45927d00aa5d0d85d35b42e888
Frank Reine <frank.reine@mac.com>
2015-12-06 23:18:37 +0100

    div

--- navtest/FPaletteVC.swift
+++ navtest/FPaletteVC.swift
@@ -17,11 +17,19 @@ class FPaletteVC: FilterEditorViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
         filter = self.editFilter as? FPalette
         strength.value = ((filter?.strength)! - 1.0) / 2.0
         filter!.reset()
         filter?.update()
         Filter.updateResultImageAsync()
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
     }
     
     @IBAction func updateStrength(sender: UISlider) {
