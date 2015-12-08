48620179a5d4f081d7a3b9098dc8923053ca9edc
uncleWalter <uncleWalter@mac.com>
2015-12-08 23:20:55 +0100

    File created

---
+++ navtest/PresetProvider.swift
@@ -0,0 +1,18 @@
+let white_c   = RGB(1,1,1) // 0
+let black_c   = RGB(0,0,0) // 1
+let red_c     = RGB(1,0,0) // 2
+let yellow_c  = RGB(1,1,0) // 3
+let green_c   = RGB(0,1,0) // 4
+let cyan_c    = RGB(0,1,1) // 5
+let blue_c    = RGB(0,0,1) // 6
+let magenta_c = RGB(1,0,1) // 7
+
+
+
+let presets = [
+    [white_c, black_c, red_c, yellow_c, green_c, cyan_c, blue_c, magenta_c], // full
+    [black_c, white_c, yellow_c, cyan_c],
+    [black_c, red_c, green_c, blue_c],
+    [white_c, black_c, blue_c]
+]
+
