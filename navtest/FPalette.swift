340c81205e879d332f8a0530d0cd0dae805c48f5
uncleWalter <uncleWalter@mac.com>
2015-12-08 12:17:00 +0100

    start presets

--- navtest/FPalette.swift
+++ navtest/FPalette.swift
@@ -8,17 +8,19 @@
 
 import UIKit
 
-let black_c   = RGB(0,0,0)
-let white_c   = RGB(1,1,1)
-let red_c     = RGB(1,0,0)
-let green_c   = RGB(0,1,0)
-let blue_c    = RGB(0,0,1)
-let magenta_c = RGB(1,0,1)
-let yellow_c  = RGB(1,1,0)
-let cyan_c    = RGB(0,1,1)
+let white_c   = RGB(1,1,1) // 0
+let black_c   = RGB(0,0,0) // 1
+let red_c     = RGB(1,0,0) // 2
+let yellow_c  = RGB(1,1,0) // 3
+let green_c   = RGB(0,1,0) // 4
+let cyan_c    = RGB(0,1,1) // 5
+let blue_c    = RGB(0,0,1) // 6
+let magenta_c = RGB(1,0,1) // 7
+
+
 
 let presets = [
-    [black_c, white_c, red_c, green_c, blue_c, magenta_c, yellow_c, cyan_c], // full
+    [white_c, black_c, red_c, yellow_c, green_c, cyan_c, blue_c, magenta_c], // full
     [black_c, white_c, yellow_c, cyan_c],
     [black_c, red_c, green_c, blue_c],
     [white_c, black_c, blue_c]
