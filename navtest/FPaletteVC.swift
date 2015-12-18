734b6ab049d335a9790ef84b7c6a580eebe10979
uncleWalter <schimfim@mac.com>
2015-12-18 11:37:37 +0100

    restruct

--- navtest/FPaletteVC.swift
+++ navtest/FPaletteVC.swift
@@ -12,25 +12,23 @@ class FPaletteVC: FilterEditorViewController {
     
     @IBOutlet var strength: UISlider!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // All IBActions call parametersChanged
+    @IBAction func updateStrength(sender: UISlider) {
+        parametersChanged()
+    }
+    
+    // Gets called from FilterEditorViewController
+    override func setupFilterEditor() {
         let filter =  self.editFilter as FPalette
         strength.value = ((filter.strength)! - 0.5) / 3.5
         filter.parameterChanged()
-        Filter.updateResultImageAsync()
+        Filter.updateResultImageAsync()    	
     }
     
-    @IBAction func updateStrength(sender: UISlider) {
+    // gets called from FilterEditorViewController
+    override func updateParameters() {
         let filter =  self.editFilter as FPalette
         filter.strength = strength.value * 3.5 + 0.5
         Filter.updateResultImageAsync()
-    }
-    
-    override func startFilterEditor() {
-    	
-    }
-    
-    override func parametersChanged() {
-    	
     }
 }
